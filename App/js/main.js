4f0d96e080eb03adac54d790c22d81f4e891cd58
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-10-19 21:57:32 +0100

    forEach with dealy worked out

--- App/js/main.js
+++ App/js/main.js
@@ -1,5 +1,9 @@
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger')
+const nav = document.querySelector('.nav')
+const menuNav = document.querySelector('.menu-nav')
+const item = document.querySelectorAll('.menu-nav__item')
+
 
 let showMenu = false;
 
@@ -7,4 +11,13 @@ menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     hamburger.classList.toggle('open')
+    nav.classList.toggle('open')
+    menuNav.classList.toggle('open')
+    item.forEach((item, index) => {
+        setTimeout(() => {
+            item.classList.toggle('open')
+        }, index * 150);
+    });
+
+
 }
